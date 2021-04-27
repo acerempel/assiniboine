d5284283f867226ec1bc385d8eaf923da3c1c095
Alan Rempel <alan.rempel@gmail.com>
2021-04-27 10:17:09 -0400

    Adjust coloours

--- single.php
+++ single.php
@@ -36,17 +36,17 @@ while ( have_posts() ) :
 
 	$assnbn_prevnext_classes =
 		'grid grid-cols-auto-2 items-center gap-x-2'
-		. ' p-2 bg-cyan-50 border-2 border-blue-500 rounded hover:bg-cyan-100 hover:border-blue-600';
+		. ' p-2 bg-blue-50 border-2 rounded hover:bg-blue-100 border-blue-600';
 
 	$assiniboine_next = '<span class="' . $assnbn_prevnext_classes . ' pl-3 justify-items-end">';
-	$assiniboine_next .= '<span class="underline text-cyan-700">' . $assiniboine_next_label . '</span>';
-	$assiniboine_next .= '<span class="row-span-2 text-cyan-700">' . $twentytwentyone_next_icon . '</span>';
+	$assiniboine_next .= '<span class="underline text-blue-700">' . $assiniboine_next_label . '</span>';
+	$assiniboine_next .= '<span class="row-span-2 text-blue-700">' . $twentytwentyone_next_icon . '</span>';
 	$assiniboine_next .= '<span class="font-semibold text-xl text-secondary">%title</span>';
 	$assiniboine_next .= '</span>';
 
 	$assiniboine_prev = '<span class="' . $assnbn_prevnext_classes . ' pr-3 justify-items-start">';
-	$assiniboine_prev .= '<span class="row-span-2 text-cyan-700">' . $twentytwentyone_prev_icon . '</span>';
-	$assiniboine_prev .= '<span class="underline text-cyan-700">' . $assiniboine_prev_label . '</span>';
+	$assiniboine_prev .= '<span class="row-span-2 text-blue-700">' . $twentytwentyone_prev_icon . '</span>';
+	$assiniboine_prev .= '<span class="underline text-blue-700">' . $assiniboine_prev_label . '</span>';
 	$assiniboine_prev .= '<span class="font-semibold text-xl text-secondary">%title</span>';
 	$assiniboine_prev .= '</span>';
 
