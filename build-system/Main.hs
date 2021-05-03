5e33f9633e1a1f1f2d2ffc7004106232608d092c
Alan Rempel <alan.rempel@gmail.com>
2021-05-03 13:58:47 -0400

    Display notification when build finished

--- build-system/Main.hs
+++ build-system/Main.hs
@@ -1,13 +1,29 @@
+import Data.List
 import Development.Shake
-import Options.Applicative
+import System.Environment ( getArgs )
 
 mainCssFile = "style.css"
 
+defaultTargets = [mainCssFile]
+
 main = do
+  args <- getArgs
+  let targets =
+        if null args then defaultTargets else args
   shake shakeOptions $ do
-    want [mainCssFile]
+    action $ do
+      need targets
+      runAfter (showNotification targets)
     mainCssFile %> \output -> do
       templateFiles <- getDirectoryFiles "" ["**/*.php"]
       cssFiles <- getDirectoryFiles "" ["source/**/*.css"]
       need $ templateFiles <> cssFiles
       cmd_ (AddEnv "TAILWIND_MODE" "build") "pnpx postcss source/style.css -o" output
+
+showNotification built =
+    cmd_
+      "osascript -e"
+      ["display notification \"" ++ text ++ "\" with title \"" ++ title ++ "\""]
+  where
+    text = intercalate ", " built ++ " were built successfully."
+    title = "Build complete!"
