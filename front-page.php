6a4e48009e03e8e70ec889e01282fefb9bfb0b58
Alan Rempel <alan.rempel@gmail.com>
2021-05-04 09:14:16 -0400

    Fiddle with layout

--- front-page.php
+++ front-page.php
@@ -4,9 +4,9 @@ get_header();
 ?>
 
 <div class="text-lg flex flex-wrap flex-space-x-16">
-	<section class="w-prose flex-shrink min-w-72 mb-2lh">
+	<section class="w-prose flex-shrink flex-grow-2 min-w-72 mb-2lh">
 		<?php while ( have_posts() ) : the_post(); ?>
-		<article class="entry-content">
+		<article class="entry-content paragraphs">
 			<?php the_content(); ?>
 		</article>
 		<?php endwhile; ?>
